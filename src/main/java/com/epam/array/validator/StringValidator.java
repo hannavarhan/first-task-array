b7c9ee04a13f62a407e2ae778984b37d0df2d28b
hanna_varhan <hannavarhan@gmail.com>
2021-03-29 14:04:24 +0300

    add validator

--- src/main/java/com/epam/array/validator/StringValidator.java
+++ src/main/java/com/epam/array/validator/StringValidator.java
@@ -1,25 +1,14 @@
 package com.epam.array.validator;
 
-import com.epam.array.entity.ArrayEntity;
-import com.epam.array.exception.ArrayException;
-import com.epam.array.exception.ArrayValidatorException;
-
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringValidator {
 
-    private static final String IS_NUMBER = "^([-+])?[0-9]*$";
-    private static Pattern pattern = Pattern.compile(IS_NUMBER);
-
-    public static ArrayEntity validate(String line) throws ArrayValidatorException, ArrayException {
-        ArrayEntity arrayEntity = new ArrayEntity(line.split(" ").length);
-        for (int i = 0; i < line.split(" ").length; i++) {
-            if (pattern.matcher(line.split(" ")[i].trim()).find()) {
-                arrayEntity.set(i, Integer.parseInt(line.split(" ")[i]));
-            } else {
-                throw new ArrayValidatorException("Line is not valid");
-            }
-        }
-        return arrayEntity;
+    public static boolean validate(String line) {
+        final String IS_VALID_STRING = "^([-+])?[0-9]+(\\s(([-+])?[0-9]+))*$";
+        Pattern pattern = Pattern.compile(IS_VALID_STRING);
+        Matcher matcher = pattern.matcher(line.trim());
+        return matcher.find();
     }
 }
