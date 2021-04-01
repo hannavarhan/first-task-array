59fc303ba29c1efcccbfcaa377ac3d5e3ff82cfa
hanna_varhan <hannavarhan@gmail.com>
2021-04-01 11:03:55 +0300

    update replace service

--- src/main/java/com/epam/array/service/impl/ReplaceServiceImpl.java
+++ src/main/java/com/epam/array/service/impl/ReplaceServiceImpl.java
@@ -3,7 +3,6 @@ package com.epam.array.service.impl;
 import com.epam.array.entity.ArrayEntity;
 import com.epam.array.exception.ArrayException;
 import com.epam.array.service.ReplaceService;
-import com.epam.array.service.SearchService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,6 +11,10 @@ public class ReplaceServiceImpl implements ReplaceService {
     private final static Logger logger = LogManager.getLogger(ReplaceServiceImpl.class);
 
     public ArrayEntity replacePositive(ArrayEntity arrayEntity) throws ArrayException {
+        if (arrayEntity == null) {
+            logger.error("Array is null in replacePositive method");
+            throw new ArrayException("Array is null");
+        }
         ArrayEntity newMyIntArray = new ArrayEntity(arrayEntity.size());
         for (int i = 0; i < arrayEntity.size(); i++) {
             newMyIntArray.set(i, arrayEntity.get(i) > 0 ? -arrayEntity.get(i) : arrayEntity.get(i));
@@ -21,6 +24,10 @@ public class ReplaceServiceImpl implements ReplaceService {
     }
 
     public ArrayEntity replaceNegative(ArrayEntity arrayEntity) throws ArrayException {
+        if (arrayEntity == null) {
+            logger.error("Array is null in replaceNegative method");
+            throw new ArrayException("Array is null");
+        }
         ArrayEntity newMyIntArray = new ArrayEntity(arrayEntity.size());
         for (int i = 0; i < arrayEntity.size(); i++) {
             newMyIntArray.set(i, arrayEntity.get(i) < 0 ? -arrayEntity.get(i) : arrayEntity.get(i));
